eca1622809d88f6208ecdeba69c54a7d3e0dfa4c
manoj-s2006 <manoj20it06@gmail.com>
2026-07-13 20:08:40 +0530

    day1

--- patternA.java
+++ patternA.java
@@ -1,10 +1,14 @@
+import java.util.*;
 class patternA {
     public static void main(String[] args) {
-        int rows = 5;
-
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        //outer for loop for rows
+        for(int i=1;i<=n;i++){
+            //inner for loop for columns
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
